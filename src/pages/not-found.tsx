80dce85fb390c0b47ca47545232134889397e5ab
DamifeZion <damifemartins@gmail.com>
2024-10-06 23:53:03 +0100

    feat: completred the 404 page

--- src/pages/not-found.tsx
+++ src/pages/not-found.tsx
@@ -1,5 +1,22 @@
+import { Button } from "@/components/ui/button";
+import Typography from "@/components/ui/typography";
+import { routeConstants } from "@/constants/route-const";
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
-   return <div>Page Not Found</div>;
+   return (
+      <div className="container w-screen h-screen flex flex-col items-center justify-center text-center">
+         <Typography variant="h2">
+            404 Not Found <br /> This page does not exist. <br />
+         </Typography>
+
+         <Button asChild className="mt-4">
+            <Link to={routeConstants.dashboard}>
+               Go to Dashboard
+            </Link>
+         </Button>
+      </div>
+   );
 };
 
 export default NotFound;
