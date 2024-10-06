7ba8b588e9f396ead9f004622a2b8acd156203b1
DamifeZion <damifemartins@gmail.com>
2024-10-06 19:49:30 +0100

    feat: Completed the navbar for the mobile screen

--- src/components/navigation/navbar.tsx
+++ src/components/navigation/navbar.tsx
@@ -1,15 +1,34 @@
+import { HamburgerMenuIcon } from "@radix-ui/react-icons"
 import { Logo } from "../logo"
+import { Button } from "../ui/button"
+import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet"
+import { Sidebar } from "./sidebar"
 
 
 export const Navbar = () => {
 
    return (
       <nav className="border border-greed-600">
-         <header className="container h-16 flex items-center">
-            <Logo />
+         <Sheet>
+            <SheetTrigger asChild>
+               <span className="container h-14 flex items-center justify-between">
+                  <img
+                     src="/dashboard/cs1.png"
+                     className="size-6 rounded-sm"
+                  />
 
-            Navbar mobile
-         </header>
+                  <Logo className="text-xl" />
+
+                  <Button size="icon" variant="ghost" className="text-primary *:size-6">
+                     <HamburgerMenuIcon />
+                  </Button>
+               </span>
+            </SheetTrigger>
+
+            <SheetContent side="left" className="px-0 py-0">
+               <Sidebar />
+            </SheetContent>
+         </Sheet>
       </nav>
    )
 }
